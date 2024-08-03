31954ce47b941c1e03c51b6f953311071f28c781
Abhilasha <abhilasha4823@gmail.com>
2024-08-03 10:30:03 +0530

    second commit

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.post("/upload", upload.single('product'), (req, res) => {
     }
     res.json({
         success: true,
-        image_url: `http://localhost:${port}/images/${req.file.filename}`
+        image_url: `https://e-commerce-backend-9goh.onrender.com/images/${req.file.filename}`
     });
 });
 
